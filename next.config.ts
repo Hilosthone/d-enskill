ff3ee87c0252e8bc7b489e64f62c4b797dd37107
hilosthone <solihullahsulyman@gmail.com>
2026-07-16 12:18:25 +0100

    Fix 404 routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  images: { unoptimized: true }, // Required for static export
+}
 
-export default nextConfig;
+export default nextConfig
